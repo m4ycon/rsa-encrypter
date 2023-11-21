e0ad0f59fbcab47e62cd116955447e851d6695dc
Maycon Fabio <m.vinnicyus@gmail.com>
2023-11-20 21:35:37 -0300

    Refactor prime number generation and add fast exponentiation

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 from random import randint
 
-def genPrime() -> int:
-  prime = 0
-  notPrimes = []
-  while not isPrime(prime):
-    notPrimes.append(prime)
-    while prime in notPrimes:
-      prime = randint(1e6, 1e7-1)
+
+def exp_mod(a: int, b: int, n: int) -> int:
+  # (a ^ b) % n with fast exponentiation
+  res = 1
+  while b > 0:
+    if b % 2 == 1:
+      res = (res * a) % n
+    a = (a * a) % n
+    b = b // 2
+  return res
+
+
+def gen_prime() -> int:
+  prime = randint(2e16, 2e17-1)
+  not_primes = []
+  while not is_prime(prime):
+    not_primes.append(prime)
+
+    while prime in not_primes:
+      prime = randint(2e16, 2e17-1)
 
   return prime
 
-def isPrime(n: int) -> bool:
+
+def is_prime(n: int) -> bool:
   # miller-rabin
   # https://www.youtube.com/watch?v=qdylJqXCDGs&ab_channel=Theoretically
-  k = 0
-  while isinstance((n - 1) / (2 ** k), int):
+  k = 1
+  while (n - 1) / (2 ** k) == int((n - 1) / (2 ** k)):
     k += 1
-  m = (n - 1) / (2 ** k)
+  k -= 1
+  m = int((n - 1) / (2 ** k))
   a = randint(2, n - 2)
 
-  a_, b = a, 0
-  for _ in range(0, m - 1): # b = (a ** m) % n
-    b = (a_ * a) % n
+  b = exp_mod(a, m, n)
+  return b == 1 or b == n - 1
+  # TODO: check next bn's to confirm prime
 
-  return False
 
 def gcd(a: int, b: int) -> int:
   if a == 0:
     return b
   return gcd(b % a, a)
 
+
 def generate_key(p: int, q: int):
+  # https://www.youtube.com/watch?v=oOcTVTpUsPQ&ab_channel=EddieWoo
   n = p * q
   phi = (p - 1) * (q - 1)
 
@@ -51,16 +67,18 @@ def generate_key(p: int, q: int):
   
   return { 'public': (e, n), 'private': (d, n) }
 
+
 def encrypt():
   return
+
 
 def decrypt():
   return
 
 
 def main():
-  res = generate_key(2, 7)
-  print(res)
-  return
+  # res = generate_key(2, 7)
+  prime = gen_prime()
+  print(f'-> {prime}')
 
 main()
