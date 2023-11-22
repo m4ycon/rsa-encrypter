dcf5527d7754ad8c56f0b3b9becea2cd38247b01
Maycon Fabio <m.vinnicyus@gmail.com>
2023-11-21 22:32:26 -0300

    Base for padding plaintext and some refactors on key type

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from random import randint
 from math import gcd
+from typing import Dict, Tuple
+
+class Key():
+  k: int
+  n: int
+
+  def __init__(self, key: int, n: int):
+    self.k = key
+    self.n = n
+
+  def __str__(self):
+    return f'({self.k}, {self.n})'
 
 
-def gen_prime() -> int:
+TKeysDict = Dict[str, Key]
+
+
+def generate_prime() -> int:
   lower_bound, upper_bound = pow(2, 1024), pow(2, 1025) - 1
-  prime = randint(lower_bound, upper_bound)
+  prime = 0
   while not is_prime(prime):
     prime = randint(lower_bound, upper_bound)
 
@@ -44,8 +59,10 @@ def is_prime(n: int) -> bool:
   return True
 
 
-def generate_key(p: int, q: int):
+def generate_keys() -> TKeysDict:
   # https://www.youtube.com/watch?v=oOcTVTpUsPQ&ab_channel=EddieWoo
+  p, q = generate_prime(), generate_prime()
+
   n = p * q
   phi = (p - 1) * (q - 1)
 
@@ -61,20 +78,28 @@ def generate_key(p: int, q: int):
 
   d += phi * randint(1e2, 1e6) # add some random number to d, still attending to be modular inverse
 
-  return { 'public': (e, n), 'private': (d, n) }
+  return { 'public': Key(key=e, n=n), 'private': Key(key=d, n=n) }
 
+def oaep(plaintext: str) -> str:
+  return plaintext
 
-def encrypt():
-  return
-
-
-def decrypt():
-  return
+def crypt(plaintext: str, key: Key) -> str:
+  paddedtext = oaep(plaintext)
+  ciphertext = ''
+  for i in paddedtext:
+    cipher_i = pow(ord(i), key.k, key.n)
+    print(hex(cipher_i))
+    ciphertext += chr(cipher_i)
+  return ciphertext
 
 
 def main():
-  p1, p2 = gen_prime(), gen_prime()
-  res = generate_key(p1, p2)
-  print(res)
+  keys_pair = generate_keys()
+
+  plaintext = 'rango brabo'
+  ciphertext = crypt(plaintext, keys_pair['public'])
+  deciphertext = crypt(ciphertext, keys_pair['private'])
+  print(f'ciphertext: {ciphertext}')
+  print(f'deciphertext: {deciphertext}')
 
 main()
